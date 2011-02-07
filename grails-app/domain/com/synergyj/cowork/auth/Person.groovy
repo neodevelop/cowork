d1c6925fce1c408dbbfc172c4329803272ca1274
Domingo Suarez Torres <domingo.suarez@gmail.com>
2011-02-07 13:38:54 -0600

    fixed some properties to became optional

--- grails-app/domain/com/synergyj/cowork/auth/Person.groovy
+++ grails-app/domain/com/synergyj/cowork/auth/Person.groovy
@@ -32,17 +32,19 @@ class Person {
     String direccionFiscal
     String rfc
     String email
+    Date dateCreated
 
 	static constraints = {
 		username blank: false, unique: true
 		password blank: false
         nombreReal blank:false
         apellidoPaterno blank:false
-        apellidoMaterno blank:false
-        razonSocial blank:false
-        direccionFiscal blank:true,size:0..500
-        rfc blank:false,size:10..13, matches:"(^([A-Z]{4}))(([0-9]{6}))((([A-Z]|[0-9]){3})|())"
-        email email:true
+        apellidoMaterno nullable:true, blank:true
+        razonSocial nullable:true, blank:true
+        direccionFiscal nullable:true, blank:true,size:0..500
+        rfc nullable:true, size:10..13, matches:"(^([A-Z]{4}))(([0-9]{6}))((([A-Z]|[0-9]){3})|())"
+        email email:true, blank:false
+        dateCreated display:false
 	}
 
 	static mapping = {
